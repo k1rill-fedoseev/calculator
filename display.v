ebd809c00b44239ef9fc2c30dd509f697a678dbd
Kirill Fedoseev <k.fedoseev@innopolis.university>
2018-10-16 16:23:46 +0300

    .gitignore

---
+++ display.v
@@ -0,0 +1,9 @@
+module display (
+input clk;
+output [7:0]ctrl;
+output [7:0]leds
+);
+
+
+
+endmodule
---
+++ numpad.v
@@ -0,0 +1,8 @@
+module numpad (
+	input clk;
+	output [3:0]reg out
+);
+
+//bla bla bla
+
+endmodule
